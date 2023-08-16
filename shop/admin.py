229ee5aa1fee2fe3ef1271084ee5a3488b6d5c14
Nikita Ostapenko <wnikx79@gmail.com>
2023-08-16 13:02:28 +0200

    create admin panel and update models

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['category_name']
+    prepopulated_fields = {'slug': ('category_name',)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['product_name',
+                    'price', 'available']
+    list_editable = ['price', 'available']
+    list_filter = ['category', 'available']
+    prepopulated_fields = {'slug': ('product_name',)}
--- shop/models.py
+++ shop/models.py
@@ -3,8 +3,17 @@ from datetime import date
 
 
 class Category(models.Model):
-    category_name = models.CharField(max_length=200)
+    category_name = models.CharField(
+        max_length=200, verbose_name='Имя категории')
     slug = models.SlugField(max_length=200, unique=True)
+
+    class Meta:
+        ordering = ['id']
+        verbose_name = 'Категории'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.category_name
 
 
 class Product(models.Model):
@@ -15,17 +24,26 @@ class Product(models.Model):
         (KILOGGRAM, 'килограмм'),
     ]
 
-    product_name = models.CharField(max_length=200)
+    product_name = models.CharField(max_length=200, verbose_name='Имя товара')
     slug = models.SlugField(max_length=200)
     category = models.ForeignKey(
-        Category, related_name='category', on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='products/')
-    description = models.TextField(blank=True)
+        Category, related_name='category', on_delete=models.CASCADE, verbose_name='Категория')
+    image = models.ImageField(upload_to='products/',
+                              verbose_name='Изображение')
+    description = models.TextField(blank=True, verbose_name='Описание')
     price = models.DecimalField(max_digits=10,
-                                decimal_places=2)
-    min_volume = models.DecimalField(max_digits=2,
-                                     decimal_places=1)
+                                decimal_places=2, verbose_name='Цена')
+    min_volume = models.DecimalField(max_digits=3,
+                                     decimal_places=2, verbose_name='Мин. кол-во')
     unit = models.CharField(max_length=2,
-                            choices=MEASURMENT_UNIT_CHOICES, default=KILOGGRAM)
-    available = models.BooleanField(default=True)
-    create = models.DateField(default=date.today)
+                            choices=MEASURMENT_UNIT_CHOICES, default=KILOGGRAM, verbose_name='Ед. измерения')
+    available = models.BooleanField(default=True, verbose_name='Доступность')
+    create = models.DateField(default=date.today, verbose_name='Дата создания')
+
+    class Meta:
+        verbose_name = 'Товары'
+        verbose_name_plural = 'Товары'
+        ordering = ['category']
+
+    def __str__(self):
+        return self.product_name
