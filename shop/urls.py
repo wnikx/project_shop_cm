6a69db1282409f695ede1c846b9f03f43b293c45
Nikita Ostapenko <wnikx79@gmail.com>
2023-08-23 12:39:00 +0300

    create some templates

--- shop/urls.py
+++ shop/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-
+from . import views
 
 urlpatterns = [
     path('', views.main_page, name='main_page'),
-    path('<category>/', views.list_categories, name='list_categories'),
+    path('products/', views.list_categories, name='list_categories'),
+    path('products/<slug:category>/',
+         views.category_products, name='category_products'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from .models import Product, Category
 
 
 def main_page(request):
-    return render(request, 'shop/main/base.html')
+    return render(request, 'shop/main/main_page.html')
+
+
+def list_categories(request):
+    products_list = Category.objects.all()
+    return render(request, 'shop/main/products.html',
+                  {'products': products_list})
+
+
+def category_products(request, category):
+    category = Category.objects.filter(slug=category)
+    products = Product.objects.filter(category=category.category_sid)
+    return render(request, 'shop/main/category_products.html',
+                  {'products': products,
+                   'category': category})
